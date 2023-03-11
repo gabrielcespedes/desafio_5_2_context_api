0e9c598f20c9b504a3fad7eaf707e0c4ac76e855
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-03-11 20:06:17 -0300

    version estable

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import "./styles.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// useState, useEffect, Context ////////////
+import { useState, useEffect } from "react";
+import MyContext from "./my_context";
+////////////////////////////////////////////
+
 import Navbar from "./components/Navbar";
 
 import Home from "./views/Home";
 import Favoritos from "./views/Favoritos";
 
 export default function App() {
-  const endpoint = "/fotos.json"; 
+  const endpoint = "/fotos.json";
+  
+  const [fotos, setFotos] = useState([]);
+  const estadoCompartido = {fotos, setFotos};
+  
+  const fotosApi = async () => {
+    const responseFotos = await fetch(endpoint);
+    const responseFotosJSON = await responseFotos.json();
+    setFotos(responseFotosJSON.photos)
+  }
+
+  useEffect(() => {
+    fotosApi()
+  }, [])
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favoritos" element={<Favoritos />} />
-        </Routes>
-      </BrowserRouter>
+      <MyContext.Provider value={estadoCompartido}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>      
     </div>
   );
 }
--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,11 +1,29 @@
 import "../assets/css/galeria.css";
 import Heart from "./Heart";
 
+// Importación useContext y MyContext
+import { useContext } from "react";
+import MyContext from "../my_context";
+///////////////////////////////////////
+
 export default function Home() {
+  const {fotos, setFotos} = useContext(MyContext);
+
+  const Heart_Click = (id) => {    
+    const pic_id = fotos.findIndex((pic) => pic.id === id);
+    fotos[pic_id].liked = !fotos[pic_id].liked;
+    setFotos([...fotos]);    
+  };
 
   return (
     <div className="galeria grid-columns-5 p-3">
-      
+      {fotos.map(pic => (        
+          <div key={pic.id} className='foto' style={{backgroundImage:`url(${pic.src.medium})`}}>
+            <Heart filled={(pic.liked)} onClick={() => Heart_Click(pic.id)}></Heart>
+            <p>{pic.alt}</p>                  
+          </div>) 
+      )
+      }
     </div>
   );
 }
